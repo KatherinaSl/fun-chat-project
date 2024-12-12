872d99911521436612f33fa31f89230ccff13bd2
KatherinaSl <106989490+KatherinaSl@users.noreply.github.com>
2024-12-12 13:57:06 +0500

    feat: base project configuration

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable no-undef */
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const EslintPlugin = require('eslint-webpack-plugin')
+
+module.exports = {
+  //   mode: "development",
+  entry: {
+    main: path.resolve(__dirname, './src/index.ts'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+      favicon: path.join(__dirname, 'src', 'favicon.ico'),
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets'), // путь к папке, где лежат картинки
+          to: path.resolve(__dirname, 'dist/img'), // куда будут копированы
+        },
+      ],
+    }),
+    new EslintPlugin({ extensions: ['ts'] }),
+  ],
+  devServer: {
+    open: true,
+    host: 'localhost',
+    hot: false,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jpg|jpeg|gif)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(css|scss)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.ts$/i,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.(svg|png|jp2|webp)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      img: path.join(__dirname, 'src', 'assets'),
+    },
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  //   optimization: {
+  //     runtimeChunk: "single",
+  //   },
+}
