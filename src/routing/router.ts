55884ee894ae80869403c736715f806b377047d7
Katherina <106989490+KatherinaSl@users.noreply.github.com>
2024-12-26 15:13:21 +0500

    feat: history routing implemented (#6)

---
+++ src/routing/router.ts
@@ -0,0 +1,47 @@
+import Pages from './pages';
+import HistoryRouterHandler from './history-router-handler';
+
+type Route = {
+  path: string;
+  callback: (path: string) => void;
+};
+
+export default class Router {
+  private routes: Array<Route>;
+
+  private handler;
+
+  constructor(routes: Array<Route>) {
+    this.routes = routes;
+    this.handler = new HistoryRouterHandler(this.urlChangedHandler.bind(this));
+
+    document.addEventListener('DOMContentLoaded', () => {
+      this.handler.navigate('');
+    });
+  }
+
+  public navigate(url: string) {
+    this.handler.navigate(url);
+  }
+
+  // redo
+  private urlChangedHandler(path: string) {
+    const route = this.routes.find((item) =>
+      item.path === path ? path : Pages.LOGIN,
+    );
+
+    if (!route) {
+      this.redirectToLoginPage();
+      return;
+    }
+
+    route.callback(path);
+  }
+
+  private redirectToLoginPage() {
+    const route = this.routes.find((item) => item.path === Pages.LOGIN);
+    if (route) {
+      this.navigate(route.path);
+    }
+  }
+}
