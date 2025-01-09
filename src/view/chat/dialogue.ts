128b00c38425b7e9df147150f75424cd25084878
Katherina <106989490+KatherinaSl@users.noreply.github.com>
2025-01-09 15:52:38 +0500

    feat: display user status in chat implemented (#12)

--- src/view/chat/dialogue.ts
+++ src/view/chat/dialogue.ts
@@ -1,39 +1,45 @@
+import { User } from '../../data/interfaces';
 import createHTMLElement from '../../util/element-creator';
 import './chat.scss';
+import * as Constants from '../../constants';
 
 export default class DialogueView {
-  private user?: string;
+  private user?: User;
 
-  CHOOSE_USER_MSG = 'Select a chat to start messaging...';
-
-  START_DIALOGUE_MSG = 'Write your first message...';
-
-  PLACEHOLDER = 'Write a message...';
-
-  constructor(user?: string) {
+  constructor(user?: User) {
     this.user = user;
   }
 
   public create() {
     const messageField = createHTMLElement('div', 'main__chat');
     const userInfo = createHTMLElement('div', 'main__chat__user');
-    const userName = createHTMLElement('p');
-    userInfo.append(userName);
+    const userName = createHTMLElement('p', 'user-name');
+    const userStatus = createHTMLElement('p', 'user-status');
+    userInfo.append(userName, userStatus);
     const dialogue = createHTMLElement('div', 'main__chat__dialogue');
     const selectUserElement = createHTMLElement('p');
     dialogue.append(selectUserElement);
     const msgField = createHTMLElement('div', 'main__chat__msg');
     const msgInput = createHTMLElement('input') as HTMLInputElement;
     msgInput.setAttribute('type', 'text');
-    msgInput.setAttribute('placeholder', this.PLACEHOLDER);
+    msgInput.setAttribute('placeholder', Constants.MSG_PLACEHOLDER);
     const sendButton = createHTMLElement('button') as HTMLButtonElement;
-    sendButton.textContent = 'Send';
+    sendButton.textContent = Constants.BUTTONS.SEND_BUTTON;
 
     if (this.user) {
-      userName.textContent = this.user;
-      selectUserElement.textContent = this.START_DIALOGUE_MSG;
+      // const status = document
+      //   .querySelector('.main__users li')?.getAttribute('status');
+      // console.log(status);
+      // userStatus.textContent =
+      //   status === 'true' ? Constants.USER_STATUS.ONLINE
+      //     : Constants.USER_STATUS.OFFLINE;
+      userStatus.textContent = this.user.isLogined
+        ? Constants.USER_STATUS.ONLINE
+        : Constants.USER_STATUS.OFFLINE;
+      userName.textContent = this.user.login;
+      selectUserElement.textContent = Constants.START_DIALOGUE_MSG;
     } else {
-      selectUserElement.textContent = this.CHOOSE_USER_MSG;
+      selectUserElement.textContent = Constants.CHOOSE_USER_MSG;
       sendButton.disabled = true;
       msgInput.disabled = true;
     }
@@ -41,5 +47,15 @@ export default class DialogueView {
     msgField.append(msgInput, sendButton);
     messageField.append(userInfo, dialogue, msgField);
     return messageField;
+  }
+
+  public static updateUserStatus() {
+    const status = document
+      .querySelector('.main__users li')
+      ?.getAttribute('status');
+    document.querySelector('.user-status')!.textContent =
+      status === 'true'
+        ? Constants.USER_STATUS.ONLINE
+        : Constants.USER_STATUS.OFFLINE;
   }
 }
