5d53d91d3ab7b471c57dbd84718b347ca9ca8611
KatherinaSl <106989490+KatherinaSl@users.noreply.github.com>
2025-01-16 16:31:05 +0500

    feat: refresh page handling implemented

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ export default class App {
 
   private aboutView: AboutView;
 
-  private chatService: UserService;
+  private userService: UserService;
 
   private websocket: WebSocketClient;
 
@@ -29,65 +29,37 @@ export default class App {
     const routes = this.createRoutes();
     this.router = new Router(routes);
     this.websocket = new WebSocketClient('ws://localhost:4000');
-    this.chatService = new UserService(this.websocket);
+    this.userService = new UserService(this.websocket);
     this.messageService = new ChatMessageService(this.websocket);
     const registry = new HandlersRegistry(this.websocket);
-    this.loginView = new LoginView(this.router, this.chatService, registry);
+    this.loginView = new LoginView(this.router, this.userService, registry);
     this.chatView = new ChatView(
-      this.chatService,
+      this.userService,
       this.messageService,
       registry,
     );
     this.aboutView = new AboutView(this.router);
-    console.log('app constructor');
   }
 
-  public start() {
-    // this.createLoginView();
-  }
+  public start() {}
 
   public createRoutes() {
     return [
       {
         path: ``,
-        callback: () => {
-          if (sessionStorage.length) {
-            this.router.navigate(`${Pages.CHAT}`);
-          } else {
-            this.createLoginView();
-          }
-        },
+        callback: () => this.grantAccessToUsers(),
       },
       {
         path: `${Pages.LOGIN}`,
-        callback: () => {
-          if (sessionStorage.length) {
-            this.router.navigate(`${Pages.CHAT}`);
-          } else {
-            this.createLoginView();
-          }
-        },
+        callback: () => this.grantAccessToUsers(),
       },
       {
         path: `${Pages.CHAT}`,
-        callback: () => {
-          document.querySelector('main')?.remove();
-          const header = new Header(this.router, this.chatService);
-          header.create();
-          document.querySelector('body')!.append(this.chatView.create());
-          const footer = new Footer();
-          footer.create();
-        },
+        callback: () => this.createChatView(),
       },
       {
         path: `${Pages.ABOUT}`,
-        callback: () => {
-          document.querySelector('main')?.remove();
-          document.querySelector('header')?.remove();
-          document.querySelector('footer')?.remove();
-
-          document.querySelector('body')!.append(this.aboutView.create());
-        },
+        callback: () => this.createAboutPageView(),
       },
     ];
   }
@@ -95,12 +67,31 @@ export default class App {
   private createLoginView() {
     document.querySelector('body')!.append(this.loginView.create());
   }
-}
 
-window.addEventListener('beforeunload', () => {
-  // const websocket = new WebSocketClient('ws://localhost:4000');
-  // const chatService = new ChatService(websocket);
-  // const messageService = new MessageService(websocket);
-  // const chatView = new ChatView(chatService, messageService);
-  // const dialogueView = new DialogueView(messageService, user);
-});
+  private createChatView() {
+    document.querySelector('header')?.remove();
+    document.querySelector('footer')?.remove();
+    const footer = new Footer();
+    const header = new Header(this.router, this.userService);
+    document.querySelector('main')?.remove();
+    header.create();
+    document.querySelector('body')!.append(this.chatView.create());
+    footer.create();
+  }
+
+  private createAboutPageView() {
+    document.querySelector('main')?.remove();
+    document.querySelector('header')?.remove();
+    document.querySelector('footer')?.remove();
+
+    document.querySelector('body')!.append(this.aboutView.create());
+  }
+
+  private grantAccessToUsers() {
+    if (sessionStorage.length) {
+      this.router.navigate(`${Pages.CHAT}`);
+    } else {
+      this.createLoginView();
+    }
+  }
+}
