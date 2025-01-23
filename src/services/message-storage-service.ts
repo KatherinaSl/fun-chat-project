776d3ab6ae24f01254d8c7ff36b355a92710ee35
Katherina <106989490+KatherinaSl@users.noreply.github.com>
2025-01-23 11:35:41 +0500

    feat/implement delivery status (#16)
    * feat: contacts view implemented
    
    * feat: message storage implemented
    
    * feat: delivery status implemented

---
+++ src/services/message-storage-service.ts
@@ -0,0 +1,78 @@
+import { Message, User } from '../data/interfaces';
+import HandlersRegistry from './handlers-registry';
+
+export default class MessageStorageService {
+  private storage = new Map<string, Message[]>();
+
+  private newMessageListener?: (message: Message) => void;
+
+  private deliveredMessageListener?: (message: Message) => void;
+
+  constructor(registry: HandlersRegistry) {
+    registry.addMessageHandler('MSG_SEND', (msg) => {
+      const incomingMsg = msg.payload?.message;
+      const currentUser = sessionStorage.getItem('user');
+      if (currentUser === incomingMsg?.from) {
+        this.storeMessageHistory(incomingMsg, incomingMsg.to!);
+      } else {
+        this.storeMessageHistory(incomingMsg!, incomingMsg!.from!);
+      }
+
+      if (this.newMessageListener) {
+        this.newMessageListener(incomingMsg!);
+      }
+    });
+
+    registry.addMessageHandler('MSG_FROM_USER', (msg) => {
+      const msgHistory = msg.payload?.messages;
+      const currentUser = sessionStorage.getItem('user');
+
+      if (!msgHistory || !msgHistory.length) {
+        return;
+      }
+
+      if (currentUser === msgHistory[0].from) {
+        this.storage.set(msgHistory[0].to!, msgHistory);
+      } else {
+        this.storage.set(msgHistory[0].from!, msgHistory);
+      }
+    });
+
+    registry.addMessageHandler('MSG_DELIVER', (msg) => {
+      const msgId = msg.payload?.message?.id;
+      const deliveredMsg = Array.from(this.storage.values())
+        .flat()
+        .filter((message) => message.id === msgId)
+        .at(0);
+      if (deliveredMsg && deliveredMsg.status) {
+        deliveredMsg.status.isDelivered = true;
+
+        if (this.deliveredMessageListener) {
+          this.deliveredMessageListener(deliveredMsg);
+        }
+      }
+    });
+  }
+
+  private storeMessageHistory(incomingMsg: Message, sender: string) {
+    let msgHistory = this.storage.get(sender);
+    if (!msgHistory) {
+      msgHistory = [];
+    }
+    msgHistory.push(incomingMsg);
+    this.storage.set(sender, msgHistory);
+  }
+
+  public getMessageHistory(user: User): Message[] {
+    const messages = this.storage.get(user.login);
+    return messages ? messages! : [];
+  }
+
+  public setNewMessageListener(callback: (message: Message) => void) {
+    this.newMessageListener = callback;
+  }
+
+  public setDeliveredMessageListener(callback: (message: Message) => void) {
+    this.deliveredMessageListener = callback;
+  }
+}
--- src/view/chat/dialogue.ts
+++ src/view/chat/dialogue.ts
@@ -1,51 +1,42 @@
-import { Message, SocketMessage, User } from '../../data/interfaces';
+import { Message, User } from '../../data/interfaces';
 import createHTMLElement from '../../util/create-element';
 import './dialogue.scss';
 import './chat.scss';
 import * as Constants from '../../constants';
 import ChatMessageService from '../../services/chat-message-service';
-import HandlersRegistry from '../../services/handlers-registry';
 import getConvertedTime from '../../util/date-util';
 import { createDeliveredIcon } from '../../util/create-svg';
+import MessageStorageService from '../../services/message-storage-service';
 
 export default class DialogueView {
   private user?: User;
 
   private messageService: ChatMessageService;
 
-  private count: number = 0;
+  private messageStorage: MessageStorageService;
 
   constructor(
     messageService: ChatMessageService,
-    registry: HandlersRegistry,
+    messageStorage: MessageStorageService,
     user?: User,
   ) {
     this.user = user;
     this.messageService = messageService;
-    registry.addMessageHandler('MSG_SEND', (msg) => {
-      if (
-        msg.payload?.message?.from === this.user?.login ||
-        msg.payload?.message?.from === sessionStorage.getItem('user')
-      ) {
-        this.showMessage(msg.payload?.message as Message);
-      } else if (msg.payload?.message?.from !== this.user?.login) {
-        //  todo redo
-        this.createMessageCounter(msg);
+    this.messageStorage = messageStorage;
+
+    this.messageStorage.setNewMessageListener((message) => {
+      if (this.user) {
+        this.showMessage(message);
       }
     });
 
-    registry.addMessageHandler('MSG_FROM_USER', (msg) => {
-      console.log(msg);
-      msg.payload?.messages?.forEach((message) => {
-        this.showMessage(message);
-        if (message.status?.isDelivered) {
-          console.log(message.text);
-        }
-      });
-    });
-
-    registry.addMessageHandler('MSG_DELIVER', (message) => {
-      console.log(message.payload?.message?.status?.isDelivered);
+    this.messageStorage.setDeliveredMessageListener((message) => {
+      const deliveredMsgStatus = document.querySelector(
+        `#${message.id} .message__status`,
+      );
+      if (deliveredMsgStatus) {
+        deliveredMsgStatus.innerHTML += createDeliveredIcon();
+      }
     });
   }
 
@@ -57,6 +48,7 @@ export default class DialogueView {
     userInfo.append(userName, userStatus);
     const dialogue = createHTMLElement('div', 'main__chat__dialogue');
     const selectUserElement = createHTMLElement('p', 'welcome-message');
+    selectUserElement.textContent = Constants.CHOOSE_USER_MSG;
     dialogue.append(selectUserElement);
     const msgField = createHTMLElement('div', 'main__chat__msg');
     const msgInput = createHTMLElement(
@@ -69,36 +61,46 @@ export default class DialogueView {
     const sendButton = createHTMLElement('button') as HTMLButtonElement;
     sendButton.textContent = Constants.BUTTONS.SEND_BUTTON;
     sendButton.addEventListener('click', this.inputMessageHandler.bind(this));
-
-    if (this.user) {
-      userStatus.textContent = this.user.isLogined
-        ? Constants.USER_STATUS.ONLINE
-        : Constants.USER_STATUS.OFFLINE;
-      userName.textContent = this.user.login;
-      selectUserElement.textContent = Constants.START_DIALOGUE_MSG;
-      this.messageService.fetchMsgHistory(this.user);
-    } else {
-      selectUserElement.textContent = Constants.CHOOSE_USER_MSG;
-      sendButton.disabled = true;
-      msgInput.disabled = true;
-    }
+    sendButton.disabled = true;
+    msgInput.disabled = true;
 
     msgField.append(msgInput, sendButton);
     messageField.append(userInfo, dialogue, msgField);
     return messageField;
   }
 
-  public static updateUserStatus() {
-    const status = document
-      .querySelector('.main__users li')
-      ?.getAttribute('status');
+  public setContact(user: User) {
+    this.user = user;
+    const userStatus = document.querySelector('.user-status');
     const userName = document.querySelector('.user-name');
-    if (userName?.textContent) {
-      document.querySelector('.user-status')!.textContent =
-        status === 'true'
-          ? Constants.USER_STATUS.ONLINE
-          : Constants.USER_STATUS.OFFLINE;
+    document
+      .querySelectorAll('.message')
+      .forEach((message) => message.remove());
+    userStatus!.textContent = this.user.isLogined
+      ? Constants.USER_STATUS.ONLINE
+      : Constants.USER_STATUS.OFFLINE;
+    userName!.textContent = this.user.login;
+    if (!document.querySelector('.welcome-message')) {
+      this.createWelcomingMessage();
     }
+
+    (
+      document.querySelector('.main__chat__msg button') as HTMLButtonElement
+    ).disabled = false;
+
+    (document.querySelector('.input-message') as HTMLInputElement).disabled =
+      false;
+
+    const messageHistory = this.messageStorage.getMessageHistory(this.user!);
+    messageHistory?.forEach((message) => {
+      this.showMessage(message);
+    });
+  }
+
+  private createWelcomingMessage() {
+    const selectUserElement = createHTMLElement('p', 'welcome-message');
+    selectUserElement.textContent = Constants.START_DIALOGUE_MSG;
+    document.querySelector('.main__chat__dialogue')?.append(selectUserElement);
   }
 
   private inputMessageHandler(event: KeyboardEvent | MouseEvent) {
@@ -121,6 +123,7 @@ export default class DialogueView {
     document.querySelector('.welcome-message')?.remove();
     const messages = document.querySelector('.main__chat__dialogue');
     const msgContainer = createHTMLElement('div', 'message');
+    msgContainer.id = message.id!;
     msgContainer!.classList.add(
       `${message.to === this.user?.login ? 'message_right' : 'message_left'}`,
     );
@@ -148,17 +151,5 @@ export default class DialogueView {
     messages?.append(msgContainer);
 
     messages?.scrollTo(0, messages.scrollHeight);
-  }
-
-  private createMessageCounter(msg: SocketMessage) {
-    document.querySelectorAll('.main__users li').forEach((liItem) => {
-      if (liItem.getAttribute('login') === msg.payload?.message?.from) {
-        liItem.querySelector('.message-counter')?.remove();
-        const span = createHTMLElement('span', 'message-counter');
-        this.count += 1;
-        span.textContent = this.count.toString();
-        liItem?.append(span);
-      }
-    });
   }
 }
